0c79785a96c371e85e6e207e90be4cc748927feb
systemis <cuthinh2002@icloud.com>
2022-09-10 10:58:44 +0700

    feat: initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#191123",
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
