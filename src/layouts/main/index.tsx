3f0ad298c63ec3d18520689ca526800594d74d74
0xTraderTrou <khangtran.btc@gmail.com>
2022-09-13 18:06:30 +0700

    feat: added social meta tags

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,4 +1,3 @@
-import { Head } from "next/document";
 import { FC, ReactNode } from "react";
 import Header from "@/src/components/header";
 import Footer from "@/src/components/footer";
@@ -10,29 +9,6 @@ interface Props {
 const MainLayout: FC<Props> = ({ children }) => {
   return (
     <>
-      <>
-        <Head>
-          {/* Primary meta tags*/}
-          <title>Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING</title>
-          <meta name="title" content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING" />
-          <meta name="description"
-                content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way" />
-
-          {/* For OpenGraph/Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://cavies.xyz/" />
-          <meta property="og:title" content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING" />
-          <meta property="og:description" content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way" />
-          <meta property="og:image" content="https://cavies.xyz/assets/images/hamster-top-c.png" />
-
-          {/* For Twitter */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://cavies.xyz/" />
-          <meta property="twitter:title" content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING" />
-          <meta property="twitter:description" content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way" />
-          <meta property="twitter:image" content="https://cavies.xyz/assets/images/hamster-top-c.png" />
-        </Head>
-      </>
       <div className="main-layout">
         <Header />
         <div className="content-wrapper">{children}</div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import "../../styles/globals.css";
-import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { FC } from "react";
+import Script from "next/script";
+import Head from "next/head";
+
+import "../../styles/globals.css";
+import "../../styles/globals.scss";
 
 const AppComponent: FC<{ Component: any; pageProps: any }> = ({
   Component,
@@ -11,7 +14,155 @@ const AppComponent: FC<{ Component: any; pageProps: any }> = ({
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AppComponent {...{ Component, pageProps }} />;
+  return (
+    <>
+      <Head>
+        {/* TODO: remove this metatag once we go live */}
+        <meta name="robots" content="noindex" />
+
+        {/* Primary meta tags*/}
+        <title>Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING</title>
+        <meta
+          name="title"
+          content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING"
+        />
+        <meta
+          name="description"
+          content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way"
+        />
+
+        {/* For OpenGraph/Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cavies.xyz/" />
+        <meta
+          property="og:title"
+          content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING"
+        />
+        <meta
+          property="og:description"
+          content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way"
+        />
+        <meta
+          property="og:image"
+          content="https://cavies.xyz/assets/images/hamster-top-c.png"
+        />
+
+        {/* For Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cavies.xyz/" />
+        <meta
+          property="twitter:title"
+          content="Cavies | TRANSFORM THE FUTURE OF WEB3 GAMING"
+        />
+        <meta
+          property="twitter:description"
+          content="UNORTHODOX WEB3 GAMING INFRASTRUCTURE - Building towards mass adoption of blockchain-enabled games - the friendly way"
+        />
+        <meta
+          property="twitter:image"
+          content="https://cavies.xyz/assets/images/hamster-top-c.png"
+        />
+
+        {/*  Icon stuffs */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="https://cavies.xyz/assets/icons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="https://cavies.xyz/assets/icons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="https://cavies.xyz/assets/icons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="https://cavies.xyz/assets/icons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="https://cavies.xyz/assets/icons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="https://cavies.xyz/assets/icons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="https://cavies.xyz/assets/icons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="https://cavies.xyz/assets/icons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://cavies.xyz/assets/icons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="https://cavies.xyz/assets/icons/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://cavies.xyz/assets/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="https://cavies.xyz/assets/icons/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://cavies.xyz/assets/icons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          href="https://cavies.xyz/assets/icons/favicon.ico"
+          type="image/x-icon"
+        />
+        <link rel="manifest" href="https://cavies.xyz/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="https://cavies.xyz/assets/icons/ms-icon-144x144.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/*  Google Analytics */}
+        <Script
+          async={true}
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-RL35P8RT0R"
+        />
+        <Script strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RL35P8RT0R');`}
+        </Script>
+      </Head>
+      <AppComponent {...{ Component, pageProps }} />
+    </>
+  );
 }
 
 export default MyApp;
